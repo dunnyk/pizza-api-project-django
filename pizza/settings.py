f81f49e1f93d439ae693be31a4b42ab1c48174ac
dunnyk <danielkamau231@gmail.com>
2022-09-21 00:23:47 +0300

    Solve whitenoise error

--- pizza/settings.py
+++ pizza/settings.py
@@ -73,14 +73,14 @@ SWAGGER_SETTINGS = {
 
 
 MIDDLEWARE = [
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'pizza.urls'
